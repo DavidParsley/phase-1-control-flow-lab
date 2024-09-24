4ccf7adf5802934c712c0734dd4c16af39061453
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-09-24 11:46:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ function scuberGreetingForFeet(feet) {
 
 }
 
-function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    return 'Ok, sounds good.'
+   } else {
+  return "No go."
+}
+
+
 }
 
 function switchOnCharmFromTip(tip) {
